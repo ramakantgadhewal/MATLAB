27c6a6aeb34c167fae136a727b33b605f7ff3c84
huangyf15 <1466879176@qq.com>
2020-07-23 08:54:28 +0800

    changed: update the structure in simulator/FEM

---
+++ Simulator/FEM/bar1D/include_flags.m
@@ -0,0 +1,6 @@
+% File to include global variables
+global nsd ndof nnp nel nen neq nd
+global CArea CArea_ex E E_ex f_ex leng phi l_ex P_ex d_ex stress mag_ex
+global LM IEN x y z be
+global solver pnl0 pnl epsl crtcl_value
+global plot_truss plot_nod plot_stress plot_disp
